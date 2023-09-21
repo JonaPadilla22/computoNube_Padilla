dc4f54300998f590f9a5fee390181912efe224aa
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-09-21 15:56:22 -0700

    practica_05

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:computonube_padilla/src/splash_screen.dart';
+import 'package:computonube_padilla/src/slider_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,8 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practica 04', 
-      home: SplashScreen(), 
+      title: 'Practica 05', 
+      home: SliderPage(), 
       debugShowCheckedModeBanner: false
     );
   }
