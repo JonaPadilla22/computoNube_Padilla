bdbbe0581373a345982921a85a201379bb641af3
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-09-27 15:44:32 -0700

    practica_08

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:computonube_padilla/src/bottom_page.dart';
+import 'package:computonube_padilla/src/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,9 +7,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practica 07', 
-      home: BottomNavigatorPage(), 
+      title: 'Practica 08', 
+      home: HomePage(), 
       debugShowCheckedModeBanner: false
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica 08'),
+        elevation: 0.0,
+        centerTitle: true,
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        color: Colors.white30,
+      ),
+      drawer: Drawer(
+        child: MainDrawer() 
+      ),
     );
   }
 }
