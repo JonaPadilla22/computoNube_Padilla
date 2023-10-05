827c700b11059eb15e220eef53b9d782b062fb83
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-10-05 14:27:15 -0700

    practica_11

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:computonube_padilla/src/listview_page.dart';
 import 'package:flutter/material.dart';
+import 'package:computonube_padilla/src/shared_page.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practica 10', 
-      home: ListPage(), 
+      title: 'Practica 11', 
+      home: SharedPage(), 
       debugShowCheckedModeBanner: false
     );
   }
