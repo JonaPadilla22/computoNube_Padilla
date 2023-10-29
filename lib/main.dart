faa3056b7e445fc251075f7d42294e19f6ac614d
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-10-29 11:57:22 -0700

    practica_15

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:computonube_padilla/src/crud_page.dart';
+import 'package:computonube_padilla/src/slivers_page.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Practica 14', 
-      home: CrudPage(), 
+      title: 'Practica 15', 
+      home: SliversPage(), 
       debugShowCheckedModeBanner: false
     );
   }
