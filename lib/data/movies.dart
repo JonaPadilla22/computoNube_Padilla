577b74eaf299375ce829fa7e598862ed98689a23
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-11-05 14:29:22 -0700

    practica_17

---
+++ lib/data/movies.dart
@@ -0,0 +1,10 @@
+final List<Map<String, dynamic>> movies = [{"id":1,"title_name":"Jaguar","Genres":"Documentary","image":"http://dummyimage.com/250x106.png/dddddd/000000"},
+{"id":2,"title_name":"Cavemen","Genres":"Comedy|Romance","image":"http://dummyimage.com/199x194.png/dddddd/000000"},
+{"id":3,"title_name":"Evil That Men Do, The","Genres":"Action|Thriller","image":"http://dummyimage.com/194x242.png/cc0000/ffffff"},
+{"id":4,"title_name":"I'm Gonna Git You Sucka","Genres":"Action|Comedy","image":"http://dummyimage.com/138x214.png/cc0000/ffffff"},
+{"id":5,"title_name":"Misérables, Les","Genres":"Crime|Drama|Romance|War","image":"http://dummyimage.com/136x213.png/cc0000/ffffff"},
+{"id":6,"title_name":"New World (Shin-sae-gye)","Genres":"Thriller","image":"http://dummyimage.com/170x137.png/ff4444/ffffff"},
+{"id":7,"title_name":"Beer","Genres":"Comedy","image":"http://dummyimage.com/227x166.png/dddddd/000000"},
+{"id":8,"title_name":"Mad Miss Manton, The","Genres":"Comedy|Crime|Mystery|Romance","image":"http://dummyimage.com/224x216.png/cc0000/ffffff"},
+{"id":9,"title_name":"Under the Skin of the City (Zir-e-poost-e Shahr)","Genres":"Drama","image":"http://dummyimage.com/219x203.png/5fa2dd/ffffff"},
+{"id":10,"title_name":"Wallace & Gromit: A Close Shave","Genres":"Animation|Children|Comedy","image":"http://dummyimage.com/169x209.png/ff4444/ffffff"}];
--- lib/src/listview_page.dart
+++ lib/src/listview_page.dart
@@ -1,34 +1,82 @@
-import 'package:computonube_padilla/src/animated_page.dart';
-import 'package:computonube_padilla/src/tween_page.dart';
 import 'package:flutter/material.dart';
+import 'package:computonube_padilla/data/movies.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
-class ListViewPage extends StatelessWidget {
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  List<Map<String, dynamic>> pelis = [...movies];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Practica 16 - Animaciones'),
-        centerTitle: true,
+        title: Text("Practica 17 - Dismissed"),
+        backgroundColor: Colors.orangeAccent,
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Animated'),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => AnimatedPage()));
-            },
-          ),
-          ListTile(
-            title: Text('Tween'),
-            trailing: Icon(Icons.arrow_right),
-            onTap: () {
-              Navigator.push(context,
-                MaterialPageRoute(builder: (context) => TweenPage()));
-            },
-          )
-        ],
-      )
+      body: ListView.builder(
+        itemCount: pelis.length,
+        itemBuilder: (BuildContext context, int index){
+          final item = pelis[index];
+          return Slidable(
+            key: const ValueKey(0),
+            child: ListTile(
+              title: Text(item['title_name']),
+              subtitle: Text(item['Genres'])
+            ),
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              dismissible: DismissiblePane(onDismissed: () {}),
+              children: [
+                SlidableAction(
+                  onPressed: (BuildContext context) {
+                    onDismissed(index, 'Agregar');
+                  },
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  icon: Icons.library_add,
+                  label: 'Agregar',
+                )
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: ScrollMotion(),
+              children: [
+                SlidableAction(
+                  flex: 2,
+                  onPressed: (BuildContext context) {
+                    onDismissed(index, 'Eliminar');
+                  },
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  icon: Icons.archive,
+                  label: 'Eliminar',
+                )
+              ],
+            ),
+          );
+        }
+      ),
     );
+  }
+
+  onDismissed(int index, String accion){
+    setState(() {
+      final _snackBar = SnackBar(content: Text('Accion: $accion'));
+      pelis.removeAt(index);
+      switch (accion) {
+        case 'Agregar':
+          ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+          break;
+        case 'Eliminar':
+          ScaffoldMessenger.of(context).showSnackBar(_snackBar);
+          break;
+      }
+    });
   }
 }
